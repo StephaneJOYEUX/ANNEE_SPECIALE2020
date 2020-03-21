5ac966b39e658a0c7000a9a5c87a32c10083440e
Nero <arcorio.y@gmail.com>
2020-03-21 11:29:50 +0100

    infirmier

--- td2/src/main/java/com/iut/as/exemple/Virus.java
+++ td2/src/main/java/com/iut/as/exemple/Virus.java
@@ -10,16 +10,15 @@ public class Virus {
 		System.out.println("Je suis plus malade");
 	}
 
-<<<<<<< HEAD
-	void malade() {
-		System.out.println("Je suis malade");
-=======
 	public void malade() {
 		System.out.println("Je suis malade");
 	}
 
 	public void contamination() {
 		System.out.println("Je contamine tout le monde puisque personne ne respecte le confinement !");
->>>>>>> 511a3ad791e8a86a3e6bae4b217dc76eb4a4126c
+	}
+	
+	public void infirmier() {
+		System.out.println("Je soigne les malades");
 	}
 }
