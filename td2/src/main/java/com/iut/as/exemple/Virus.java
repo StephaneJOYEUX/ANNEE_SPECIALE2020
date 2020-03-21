511a3ad791e8a86a3e6bae4b217dc76eb4a4126c
JOYEUX <stephane.joyeux@univ-lorraine.fr>
2020-03-21 11:12:57 +0100

    Ajout de la méthode contamination.

--- td2/src/main/java/com/iut/as/exemple/Virus.java
+++ td2/src/main/java/com/iut/as/exemple/Virus.java
@@ -9,8 +9,12 @@ public class Virus {
 	public void guerison() {
 		System.out.println("Je suis plus malade");
 	}
-	
+
 	public void malade() {
 		System.out.println("Je suis malade");
+	}
+
+	public void contamination() {
+		System.out.println("Je contamine tout le monde puisque personne ne respecte le confinement !");
 	}
 }
