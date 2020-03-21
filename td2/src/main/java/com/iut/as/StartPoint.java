3445276dd5d265cafaae1d9ff1f52cefd225316b
JOYEUX <stephane.joyeux@univ-lorraine.fr>
2020-03-21 11:52:32 +0100

    test maths

--- td2/src/main/java/com/iut/as/StartPoint.java
+++ td2/src/main/java/com/iut/as/StartPoint.java
@@ -1,10 +1,14 @@
 package com.iut.as;
 
+import com.iut.as.modele.Mathematic;
+
 public class StartPoint {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World TD2");
-
+		Mathematic maths = new Mathematic();
+		System.out.println("Le resultat de 2 + 4 est " + maths.addition(2, 4));
+		System.out.println("Le resultat de 4 / 2 est " + maths.division(4, 2));
 	}
 
 }
