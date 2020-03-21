c88063a81b1ad40f6b1262737907f34f64f9c343
JOYEUX <stephane.joyeux@univ-lorraine.fr>
2020-03-21 10:34:56 +0100

    ajout de la fonction 'guerison'

--- td2/src/main/java/com/iut/as/exemple/Virus.java
+++ td2/src/main/java/com/iut/as/exemple/Virus.java
@@ -5,4 +5,12 @@ public class Virus {
 	public void confinement() {
 		System.out.println("ON RESTE CHEZ SOI !!");
 	}
+
+	public void guerison() {
+		System.out.println("Je suis plus malade");
+	}
+	
+	void malade() {
+		
+	}
 }
