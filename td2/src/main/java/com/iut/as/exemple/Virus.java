26333784c562a74b0f534737e9ed78df4bbd083a
JOYEUX <stephane.joyeux@univ-lorraine.fr>
2020-03-28 11:03:23 +0100

    add interface for class virus

--- td2/src/main/java/com/iut/as/exemple/Virus.java
+++ td2/src/main/java/com/iut/as/exemple/Virus.java
@@ -1,23 +1,30 @@
 package com.iut.as.exemple;
 
-public class Virus {
+import com.iut.as.interfaces.IVirus;
 
+public class Virus implements IVirus {
+
+	@Override
 	public void confinement() {
 		System.out.println("ON RESTE CHEZ SOI !!");
 	}
 
+	@Override
 	public void guerison() {
 		System.out.println("Je suis plus malade");
 	}
 
+	@Override
 	public void malade() {
 		System.out.println("Je suis malade");
 	}
 
+	@Override
 	public void contamination() {
 		System.out.println("Je contamine tout le monde puisque personne ne respecte le confinement !");
 	}
-	
+
+	@Override
 	public void infirmier() {
 		System.out.println("Je soigne les malades");
 	}
---
+++ td2/src/main/java/com/iut/as/interfaces/IVirus.java
@@ -0,0 +1,14 @@
+package com.iut.as.interfaces;
+
+public interface IVirus {
+
+	void confinement();
+
+	void guerison();
+
+	void malade();
+
+	void contamination();
+
+	void infirmier();
+}
