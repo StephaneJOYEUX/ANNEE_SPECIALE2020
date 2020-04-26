e58934b7cebf4c359cf5c8f1189642182b81b4ac
KatiaDehbi <katia.dehbi12@gmail.com>
2020-04-27 01:20:39 +0200

    Gestion un compte bancaire

--- bank/src/main/java/com/iut/as/modele/Bank.java
+++ bank/src/main/java/com/iut/as/modele/Bank.java
@@ -1,5 +1,83 @@
 package com.iut.as.modele;
+import java.util.*; 
+
 
 public class Bank {
+	
+	
+	public Compte compte;	
+	public Client client ; 		
+	
+	public  double tauxDeChangeEnEuro = 1.08; 	
+	public  double tauxDeChangeEnDolars = 1; 
 
+	public Bank(Compte compte, Client client) {
+		super();
+		this.compte = compte;
+		this.client = client;
+	}
+
+	public Bank() {
+		super();
+	}
+
+
+	public void  retrait(String numClient,String nomClient, double montant) {
+		  		compte.solde-= montant;
+		}
+	
+	
+	public void  depot(String numClient,String  nomClient, double montant) {
+				compte.solde += montant;
+				
+	}
+
+	public Compte  ouverturCompte(String nomClient) {
+		
+		 return new Compte(); 
+    }
+	
+	 public double Consultation(String numCompte ) {
+		 
+		   return compte.getSolde();
+		   
+		}
+	 
+	 
+	public double  conversionFromEuro( float montantReal) {
+		
+		return  montantReal * tauxDeChangeEnEuro; 
+	}
+	 
+	public double   conversionToEuro(double montantReal) {
+			
+		return montantReal* tauxDeChangeEnDolars; 
+
+	}
+
+	@Override
+	public String toString() {
+			return "Bank [compte=" + compte + ", client=" + client + "]";
+		}
+
+
+	 
+	
+	
+   
+	
+	
+	
+	
+	    
+	 
+
+
+	
+	 
+
+	
+	
+	
 }
+
