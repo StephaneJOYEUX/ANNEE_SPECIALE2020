a6eabeeb2e7e3cbe1b5d1c6c3a6b2bb34fdec4d3
JOYEUX <stephane.joyeux@univ-lorraine.fr>
2020-05-09 11:37:02 +0200

    configuration du projet - Struts2 - MVC

---
+++ bank/src/main/java/com/iut/as/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.iut.as.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/***
+ * @description : Le 1er controleur de l'application :)
+ * 
+ * @author stephane.joyeux
+ *
+ */
+public class LoginController extends ActionSupport {
+
+	private static final long serialVersionUID = 5540616014690763867L;
+
+}
